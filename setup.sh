3b651789d2698f3f34949523d8792baf75385e9f
victor.j.morrow@gmail.com <victor.j.morrow@gmail.com>
2022-04-18 09:43:11 -0400

    Attempting to build deps on fetch

--- setup.sh
+++ setup.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
-echo 'posix success'
-ls >> ls.txt
-test -f _corral/github_com_vijayee_Blake3/Blake3/c/blake3.c >> test.txt
+cd ../../
 mkdir -p build/lib
 clang -o build/lib/blake3.o -c _corral/github_com_vijayee_Blake3/Blake3/c/blake3.c
 clang -o build/lib/blake3_dispatch.o -c _corral/github_com_vijayee_Blake3/Blake3/c/blake3_dispatch.c
@@ -16,3 +14,4 @@ build/lib/blake3_avx512_x86-64_unix.o
 rm build/lib/blake3.o build/lib/blake3_dispatch.o build/lib/blake3_portable.o \
 build/lib/blake3_sse2_x86-64_unix.o build/lib/blake3_sse41_x86-64_unix.o build/lib/blake3_avx2_x86-64_unix.o \
 build/lib/blake3_avx512_x86-64_unix.o
+cd _corral/github_com_vijayee_Blake3/
